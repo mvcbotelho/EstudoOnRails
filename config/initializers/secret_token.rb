311baaa1576de1974fe58ce55838fe3ba0c77eae
Marcus <marcus.itec@gmail.com>
2014-05-19 10:32:06 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estudos::Application.config.secret_token = 'c27b9426c281df45cdb22a250991f13d6359b113cb927a415d419d3984dfbaa51d193752c33064f4a3a62686661a86138cf895b770e3078cb695ed91de8a7ba4'
